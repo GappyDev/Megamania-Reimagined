ba71e4f1a42a942e8e63a2e97a6c1d25244b3833
Daniel Rivero <danielrivero2000@gmail.com>
2019-08-07 17:14:20 -0400

    bullet movement structure update

---
+++ Megamania Reimagined Game/Assets/_Scripts/bullets/BulletBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BulletBehavior : MonoBehaviour
+{
+
+    [Header("Bullet Data")]
+    public BulletData data;
+
+    //abstract methods
+    protected abstract void initializeData();
+    protected abstract void moveBullet();
+
+    private void Awake() => initializeData();
+}
