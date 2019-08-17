c46a71f1ab9780bd94c3acabff301528d679bfc9
Daniel Rivero <danielrivero2000@gmail.com>
2019-08-16 23:34:34 -0400

    pause mechanic update

--- Megamania Reimagined Game/Assets/_Scripts/game controller/GameController.cs
+++ Megamania Reimagined Game/Assets/_Scripts/game controller/GameController.cs
@@ -8,6 +8,11 @@ public class GameController : MonoBehaviour
     private Ship[] inGameShips;
     private bool isPaused = false;
 
+    [SerializeField]
+    private GameObject[] gameHUD;
+    [SerializeField]
+    private GameObject[] pauseUI;
+
     private void getReference() => inGameShips = FindObjectsOfType<Ship>();
 
     private void delegateStop()
@@ -28,7 +33,9 @@ public class GameController : MonoBehaviour
     {
         if (isPaused)
         {
-            //deactivate pause menu
+            foreach (GameObject obj in pauseUI) obj.SetActive(false);//deactivate pause menu
+            foreach (GameObject obj in gameHUD) obj.SetActive(true); //activate digital controls
+
             Time.timeScale = 1;
             isPaused = false;
         }
@@ -36,7 +43,9 @@ public class GameController : MonoBehaviour
         {
             Time.timeScale = 0;
             isPaused = true;
-            //activate pause menu
+
+            foreach (GameObject obj in gameHUD) obj.SetActive(false); //deactivate digital Controls
+            foreach (GameObject obj in pauseUI) obj.SetActive(true);//activate pause menu
         }
 
     }
